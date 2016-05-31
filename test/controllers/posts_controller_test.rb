a58d6c3de716037b0a4ed1358ba05a2ed0806320
andrewariley87 <andrewariley87@gmail.com>
2016-05-30 21:25:09 -0400

    adds tests for both posts controller and post model

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -1,10 +1,11 @@
 require 'test_helper'
 
 class PostsControllerTest < ActionController::TestCase
+
   setup do
     @post = posts(:post_one)
   end
-  
+
   test "should get index" do
     get :index
     assert_response :success
@@ -18,10 +19,16 @@ class PostsControllerTest < ActionController::TestCase
 
   test "should create post" do
     assert_difference('Post.count') do
-      post :create, post: {  }
+      post :create, post: { title: "Title", body: "Body" }
     end
+    assert_redirected_to posts_path
+  end
 
-    assert_redirected_to post_path(assigns(:post))
+  test "should render new if post is not created" do
+    assert_no_difference('Post.count') do
+      post :create, post: { title: "Title" }
+    end
+    assert_template :new
   end
 
   test "should show post" do
@@ -35,8 +42,19 @@ class PostsControllerTest < ActionController::TestCase
   end
 
   test "should update post" do
-    patch :update, id: @post, post: {  }
-    assert_redirected_to post_path(assigns(:post))
+    patch :update, id: @post, post: { title: "New Title" }
+    post = assigns(:post)
+    assert_equal "New Title", post.title
+    assert_redirected_to posts_path
+  end
+
+  test "post will not update if title is nil" do
+    assert_equal "Post One", @post.title
+    patch :update, id: @post, post: { title: nil }
+    post = assigns(:post)
+    post.reload
+    assert_equal "Post One", post.title
+    assert_template :edit
   end
 
   test "should destroy post" do
